bec3e61105d484e206cf2fc9aa03bfe65dc88855
Juan Melguizo <122971281+juanmelmos@users.noreply.github.com>
2023-11-24 18:20:37 +0100

    Update test-cli.sh

--- test-cli.sh
+++ test-cli.sh
@@ -1,58 +1,35 @@
 #!/bin/bash
 
-# Variables
-vpc_name="VPC-MotiJuan"
+# Configuración inicial
 region="us-east-1"
-cidr_block="192.168.0.0/22"
+vpc_cidr_block="192.168.0.0/22"
+subnet_cidr_blocks=("192.168.0.0/23" "192.168.2.0/24" "192.168.3.0/25" "192.168.3.128/27")
+department_names=("ingenieria" "desarrollo" "mantenimiento" "soporte")
 
 # Crear VPC
-vpc_id=$(aws ec2 create-vpc --cidr-block $cidr_block --query 'Vpc.VpcId' --output text --region $region)
-aws ec2 create-tags --resources $vpc_id --tags Key=Name,Value=$vpc_name --region $region
-aws ec2 modify-vpc-attribute --vpc-id $vpc_id --enable-dns-support "{\"Value\":true}" --region $region
-aws ec2 modify-vpc-attribute --vpc-id $vpc_id --enable-dns-hostnames "{\"Value\":true}" --region $region
+vpc_id=$(aws ec2 create-vpc --cidr-block $vpc_cidr_block --amazon-provided-ipv6-cidr-block --query 'Vpc.VpcId' --output text --region $region)
 echo "VPC creada con ID: $vpc_id"
 
-aws ec2 wait vpc-available --vpc-ids $vpc_id
+# Habilitar la resolución DNS y asignar un nombre a la VPC
+aws ec2 modify-vpc-attribute --vpc-id $vpc_id --enable-dns-support "{\"Value\":true}" --region $region
+aws ec2 modify-vpc-attribute --vpc-id $vpc_id --enable-dns-hostnames "{\"Value\":true}" --region $region
+aws ec2 create-tags --resources $vpc_id --tags Key=Name,Value=CRUsystemVPC --region $region
 
-# Desarrollo
+# Crear subredes y máquinas EC2
+for ((i=0; i<${#subnet_cidr_blocks[@]}; i++)); do
+    subnet_cidr=${subnet_cidr_blocks[$i]}
+    department_name=${department_names[$i]}
+    
+    # Crear subred
+    subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block $subnet_cidr --availability-zone ${region}a --query 'Subnet.SubnetId' --output text --region $region)
+    echo "Subred ($department_name) creada con ID: $subnet_id"
+    
+    # Crear máquina EC2
+    instance_id=$(aws ec2 run-instances --image-id ami-0230bd60aa48260c6 --instance-type t2.micro --subnet-id $subnet_id --query 'Instances[0].InstanceId' --output text --region $region)
+    echo "Máquina EC2 ($department_name) creada con ID: $instance_id"
+    
+    # Asignar nombre a la máquina EC2
+    aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=ec2-$department_name --region $region
+done
 
-name="desarrollo"
-
-subnet_cidr="192.168.0.0/23"
-subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block $subnet_cidr --availability-zone $availability_zone --query 'Subnet.SubnetId' --output text --region $region)
-instance_name="ec2-$name"
-aws ec2 run-instances --image-id ami-0230bd60aa48260c6 --instance-type t2.micro --subnet-id $subnet_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=ec2-desarrollo}]" --region $region
-
-echo "Subred y EC2 para $name creadas"
-
-# Soporte
-
-name="soporte"
-subnet_cidr="192.168.2.0/24"
-subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block $subnet_cidr --availability-zone $availability_zone --query 'Subnet.SubnetId' --output text --region $region)
-instance_name="ec2-$name"
-aws ec2 run-instances --image-id ami-0230bd60aa48260c6 --instance-type t2.micro --subnet-id $subnet_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=ec2-soporte}]" --region $region
-
-echo "Subred y EC2 para $name creadas"
-
-# Ingeniería
-availability_zone="us-east-1a"
-name="ingenieria"
-subnet_cidr="192.168.3.0/25"
-subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block $subnet_cidr --availability-zone $availability_zone --query 'Subnet.SubnetId' --output text --region $region)
-instance_name="ec2-$name"
-aws ec2 run-instances --image-id ami-0230bd60aa48260c6 --instance-type t2.micro --subnet-id $subnet_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=ec2-ingenieria}]" --region $region
-
-echo "Subred y EC2 para $name creadas"
-
-# Mantenimiento
-
-name="mantenimiento"
-subnet_cidr="192.168.3.128/27"
-subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block $subnet_cidr --availability-zone $availability_zone --query 'Subnet.SubnetId' --output text --region $region)
-instance_name="ec2-$name"
-aws ec2 run-instances --image-id ami-0230bd60aa48260c6 --instance-type t2.micro --subnet-id $subnet_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=ec2-mantenimiento}]" --region $region
-
-echo "Subred y EC2 para $name creadas"
-
-echo "Script completado"
+echo "Script completado exitosamente"
